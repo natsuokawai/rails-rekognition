7415e130cb1be721bb2326148cc0a82dfa1f168a
natsukawai <kawai.natsuo@gmail.com>
2019-08-25 15:19:14 +0900

    add search method

--- app/models/rekognition_face.rb
+++ app/models/rekognition_face.rb
@@ -5,5 +5,10 @@ class RekognitionFace < ApplicationRecord
     def factory!(user, face_id)
       user.rekognition_faces.create(face_id: face_id)
     end
+
+    def search(image)
+      face_ids = AmazonRekognition.search_all_faces_by_image(image)
+      where(face_id: face_ids)
+    end
   end
 end
