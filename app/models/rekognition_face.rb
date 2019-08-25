1d0da264f42944728f6fdbeb24ebcaec90c20183
natsukawai <kawai.natsuo@gmail.com>
2019-08-25 16:22:31 +0900

    fix RekognitionFace

--- app/models/rekognition_face.rb
+++ app/models/rekognition_face.rb
@@ -2,13 +2,17 @@ class RekognitionFace < ApplicationRecord
   belongs_to :user
 
   class << self
-    def factory!(user, face_id)
-      user.rekognition_faces.create(face_id: face_id)
+    def factory!(user, image_path)
+      res = AmazonRekognition.index_faces(image_path)
+      raise ArgumentError, 'Persons in the picture should be one.' if res.size > 1
+
+      user.rekognition_faces.create(face_id: res.keys[0])
     end
 
-    def search(image)
-      face_ids = AmazonRekognition.search_all_faces_by_image(image)
-      where(face_id: face_ids)
+    def search(image_path)
+      faces = AmazonRekognition.search_all_faces_by_image(image_path)
+      matched_face_ids = faces.map { |face| face[:matched_face_id] }
+      where(face_id: matched_face_ids)
     end
   end
 end
