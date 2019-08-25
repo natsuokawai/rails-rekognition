01484327fcc5df87ba7cdfb72ddcda39b36f225c
natsukawai <kawai.natsuo@gmail.com>
2019-08-25 15:19:02 +0900

    modify AmazonRekognition class

--- lib/utils/amazon_rekognition.rb
+++ lib/utils/amazon_rekognition.rb
@@ -1,44 +1,51 @@
 class AmazonRekognition
-  def initialize
-    Aws.config[:region] = '*****'
-    Aws.config[:access_key_id] = '*****'
-    Aws.config[:secret_access_key] = '*****'
+  class << self
+    def index_faces(image)
+      res = client.index_faces({
+        collection_id: collection_id,
+        image: {
+          bytes: image
+        }
+      })
 
-    @client = Aws::Rekognition::Client.new
-    @collection_id = 'myphotos'
-  end
-
-  def index_faces(image)
-    res = @client.index_faces({
-      collection_id: @collection_id,
-      image: {
-        bytes: image
-      }
-    })
-
-    res.to_h[:face_records].map { |face| [face[:face_id], face[:bounding_box]] }.to_h
-  end
-
-  def search_faces(face_id)
-    res = @client.search_faces({
-      collection_id: @collection_id,
-      face_id: face_id,
-      face_match_threshold: 95
-    })
-
-    searched_face_id = res.to_h[:searched_face_id]
-    matched_face_id = res.to_h[:face_matches][0][:face][:face_id]
-    
-    { searched_face_id: searched_face_id, matched_face_id: matched_face_id }
-  end
-
-  def search_all_faces_by_image(image_path)
-    faces = index_faces(@collection_id, image_path)
-
-    matched_faces = faces.keys.map do |face_id|
-      search_faces(@collection_id, face_id)
+      res.to_h[:face_records].map { |face| [face[:face_id], face[:bounding_box]] }.to_h
     end
 
-    matched_faces.map { |face| { face_id: face[:face_id], bounding_box: face[:bounding_box]}}
+    def search_faces(face_id)
+      res = client.search_faces({
+        collection_id: collection_id,
+        face_id: face_id,
+        face_match_threshold: 95
+      })
+
+      searched_face_id = res.to_h[:searched_face_id]
+      matched_face_id = res.to_h[:face_matches][0][:face][:face_id]
+
+      { searched_face_id: searched_face_id, matched_face_id: matched_face_id }
+    end
+
+    def search_all_faces_by_image(image)
+      faces = index_faces(image)
+
+      matched_faces = faces.keys.map do |face_id|
+        search_faces(face_id)
+      end
+
+      matched_faces.map { |face| [face[:face_id], face[:bounding_box]] }.to_h
+    end
+
+    private
+
+    def client
+      @client = Aws::Rekognition::Client.new(
+                  region: 'ap-northeast-1',
+                  access_key_id: '*****',
+                  secret_access_key: '*****'
+                )
+    end
+
+    def collection_id
+      'myphotos'
+    end
   end
 end
