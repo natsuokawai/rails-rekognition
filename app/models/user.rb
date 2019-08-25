c866c15e221e1046915fae96de14b3328d28be73
natsukawai <kawai.natsuo@gmail.com>
2019-08-25 16:31:49 +0900

    add methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   has_many :rekognition_faces
+
+  class << self
+    def in_the_image(image_path)
+      faces = RekognitionFace.search(image_path)
+      where(id: faces.pluck(:user_id))
+    end
+  end
+
+  def register_face(image_path)
+    rekognition_faces.factory!(self, image_path)
+  end
 end
