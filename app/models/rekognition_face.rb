5abc5c9bb130a1ba9f29c8b3a19fdb4c12381f7a
natsukawai <kawai.natsuo@gmail.com>
2019-08-25 12:21:04 +0900

    define relation of users and rekognition faces

--- app/models/rekognition_face.rb
+++ app/models/rekognition_face.rb
@@ -1,2 +1,9 @@
 class RekognitionFace < ApplicationRecord
+  belongs_to :user
+
+  class << self
+    def factory!(user, face_id)
+      user.rekognition_faces.create(face_id: face_id)
+    end
+  end
 end
